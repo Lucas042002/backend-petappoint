9b750e7f6def5426f46601b30d964bdc4b6a37cf
Lucas042002 <lucaschavez2002@gmail.com>
2023-11-16 00:25:02 -0300

    Update index.js No se tenia puesto el password

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,8 +11,8 @@ var jsonParser = bodyParser.json();
 
 const connection = mysql.createConnection({
     host:process.env.DB_HOST ,
-    user:'root' ,
-    password:'',
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
     port:process.env.DB_PORT,
     database:process.env.MYSQL_DB
 
